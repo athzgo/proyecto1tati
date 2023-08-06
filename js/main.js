5e42c309c0ed7acac538670e4910af25baed472c
athzgo <atati300@gmail.com>
2023-08-06 17:58:15 -0300

    se agrega proyecto final

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let btat=document.querySelector("#btat");
+btat.addEventListener("click", () => btat.style.backgroundColor= "#1b1396")
+let btinicio=document.querySelector("#btinicio");
+btinicio.addEventListener("click", () => btinicio.style.backgroundColor= "#1b1396")
+let btcontactos=document.querySelector("#btcontactos");
+btcontactos.addEventListener("click", () => btcontactos.style.backgroundColor= "#1b1396")
